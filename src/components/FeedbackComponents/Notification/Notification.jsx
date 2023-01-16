b838f5f652fb0845a0673bc6484c1e044b86b815
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2023-01-16 21:42:07 +0200

    Notification

--- src/components/FeedbackComponents/Notification/Notification.jsx
+++ src/components/FeedbackComponents/Notification/Notification.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types"
-import {NotificationMessage} from "./Notification.styled"
+import {NotificationMessage} from "./NotificationMessage.styled"
 
 
 export const Notification = ({ message }) => { 
